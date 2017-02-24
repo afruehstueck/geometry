c2ed9ff241596c9f6ef7c6992aebcb71749878ec
Anna <anna.fruehstueck@gmail.com>
2017-02-24 20:41:45 +0300

    uniform laplacian working, cotan laplacian fed

--- mesh_viewer/calc_laplacian.m
+++ mesh_viewer/calc_laplacian.m
@@ -18,8 +18,6 @@ tic;
     
     for i=1:num_vertices %iterate over all vertices      
         v_i = V(i,:);
-        [adj_face_indices,~] = find(FxV(:, i));
-  
         [adj_vertices, ~] = find(VxV(:, i));
         num_verts = size(adj_vertices, 1);
         
@@ -29,17 +27,8 @@ tic;
             v_j = V(j,:);
             
             d_ji = v_j - v_i; 
-            accum = accum + d_ji;
-%             verts = F(adj_faces(f), :);
-%             v1 = V(verts(1),:);
-%             v2 = V(verts(2),:);
-%             v3 = V(verts(3),:);
-%             c = cross((v2 - v1), (v3 - v1));
-%             Ns = Ns + c;
-            %Ns(f, :) = c;% / norm(c);   
+            accum = accum + d_ji;  
         end
-        %TODO
-        %A = 3 / sum(areas);
         L(i,:) = accum / num_verts;
     end
 toc;
@@ -53,7 +42,7 @@ tic;
     
     for i=1:num_vertices %iterate over all vertices      
         v_i = V(i,:);
-        [adj_face_indices,~] = find(FxV(:, i));
+        [adj_face_indices, ~] = find(FxV(:, i));
         adj_faces = F(adj_face_indices, :);
         
         %adj_faces = sort(adj_faces, 2)
@@ -72,7 +61,7 @@ tic;
             %         /  \     /
             %        /    \   / 
             %       /      \ /
-            % v_k1 o--------o v_j
+            % v_k1 o-------o v_j
             %
             %find third vertices for faces adjacent to edge [v_i, v_j]
             [rows, ~] = find(adj_faces == j);
@@ -86,20 +75,29 @@ tic;
         
             v_k = V(v_inds,:); %get coordinates
             
-            d_ik = v_i - v_k;
-            d_jk = v_j - v_k;
-            d_ij = v_i - v_j;
-            dots = dot(d_ik, d_jk, 2); %dot products from vectors in rows
-            lengths_i = (sqrt(sum((d_ik').^2)))'; %||v_i - v_k||
-            lengths_j = (sqrt(sum((d_jk').^2)))'; %||v_j - v_k||
-            lens = lengths_i .* lengths_j; %||v_i - v_k|| * ||v_j - v_k||
-            diffs = dots ./ lens;
-            angles = acos(diffs); %angles at vertices v_k
+            ik = v_i - v_k;
+            jk = v_j - v_k;
             
-            %TODO 
-            areas = areas + (0.5 * lens .* sin(angles)); %areas of triangles
- 
-            accum(:) = accum + sum(cot(angles)) * d_ij;
+            dots = dot(ik, jk, 2); %dot products from vectors in rows
+            lengths_i = (sqrt(sum((ik').^2)))'; %row-wise norm ||v_i - v_k||
+            lengths_j = (sqrt(sum((jk').^2)))'; %row-wise norm ||v_j - v_k||
+            comb_lenghts = lengths_i .* lengths_j; %||v_i - v_k|| * ||v_j - v_k||
+            diffs = dots ./ comb_lenghts;
+            angles = acos(diffs); %angles at vertices v_k
+            cotan_sum_angles = sum(cot(angles));
+            
+            %TODO currently only considering triangle1
+            %cotangent weight scheme [Meyer '02]
+            if true%is_obtuse([v_i; v_j; v_k(1,:)])
+                %TODO wrong
+                areas = areas + sum ((0.5 * comb_lenghts .* sin(angles)) / 3); %third of area of triangles
+            else
+                length_ij = norm(v_i - v_j);
+                areas = areas + cotan_sum_angles * length_ij * length_ij;
+            end
+            
+            ji = v_j - v_i;
+            accum = accum + cotan_sum_angles * ji;
 %             verts = F(adj_faces(f), :);
 %             v1 = V(verts(1),:);
 %             v2 = V(verts(2),:);
@@ -109,8 +107,19 @@ tic;
             %Ns(f, :) = c;% / norm(c);   
         end
         %TODO
-        A = 3 / sum(areas);
-        L(i,:) = A * accum;
+        A = sum(areas); %took each triangle area twice, cancels out 2* in equation
+        %A = 3 / sum(areas);
+        L(i,:) = accum / A;
     end
 toc;
+end
+
+%see http://mathworld.wolfram.com/ObtuseTriangle.html
+function result = is_obtuse(vertices)
+    ind = [1 2; 1 3; 2 3]; %all combinations of indices
+    rem = [3; 2; 1]; %remaining point
+    edges = vertices(ind(:,2),:) - vertices(ind(:,1),:);
+    edgessq = sum(abs(edges).^2,2); %row-wise norm squared
+    compare = (edgessq(ind(:,1),:) + edgessq(ind(:,2),:) < edgessq(rem(:,1),:)); %evaluate all a^2 + b^2 < c^2
+    result = any(compare); %is any of the inequalities true
 end
--- mesh_viewer/mesh_viewer_main.m
+++ mesh_viewer/mesh_viewer_main.m
@@ -7,12 +7,12 @@ clc;
 clear;
 %[V, F] = read_obj('../data/shuttle.obj');          %  ~300 vertices
 %[V, F] = read_obj('../data/magnolia.obj');         %  ~800 vertices
-%[V, F] = read_obj('../data/teddy.obj');            % ~1500 vertices
+[V, F] = read_obj('../data/teddy.obj');            % ~1500 vertices
 %[V, F] = read_obj('../data/bunny.obj');            % ~2500 vertices
 %[V, F] = read_obj('../data/teapot.obj');           % ~3600 vertices
 %[V, F] = read_obj('../data/lamp.obj');             % ~4400 vertices
 %[V, F] = read_obj('../data/cow.obj');              % ~4500 vertices
-[V, F] = read_obj('../data/pumpkin.obj');          % ~5000 vertices
+%[V, F] = read_obj('../data/pumpkin.obj');          % ~5000 vertices
 %[V, F] = read_obj('../data/suzanne.obj');          % ~7800 vertices
 %[V, F] = read_obj('../data/trumpet.obj');          %~11900 vertices
 %[V, F] = read_obj('../data/minicooper.obj');       %big!  ~44000 vertices
@@ -28,23 +28,36 @@ FxF = calc_adjacent_faces(F);
 % figure('Name', 'Face Adjacency')
 % spy(S_f)
 
-N = calc_normal(V, F, FxV);
+% N = calc_normal(V, F, FxV);
 L = calc_laplacian('cotan', V, F, FxV, VxV);
-
-figure;
-trisurf(F, V(:, 1), V(:, 2), V(:, 3));
-
 scr = get(0, 'ScreenSize'); 
-fig = figure('Name', '2D Viewer', 'NumberTitle', 'off', 'Position', [scr(3)/2 50 scr(3)/3 scr(3)/3]);
+plt = zeros(1, 2);
+original = figure('Name', 'Laplacian', 'NumberTitle', 'off', 'Position', [scr(3)/4 50 scr(3)/2 scr(3)/4]);
+plt(1) = subplot(1,2,1);
 hold on;
-%axis vis3d
-V = V + L
-trisurf(F, V(:, 1), V(:, 2), V(:, 3));%,'FaceColor',[0.26,0.33,1.0 ]);
+%trisurf(F, V(:, 1), V(:, 2), V(:, 3));
+quiver3(V(:, 1), V(:, 2), V(:, 3), L(:, 1), L(:, 2), L(:, 3));
+view(2)
+plt(2) = subplot(1,2,2);
+axis(plt, 'vis3d');
+grid off;
+for i = 1:10
+    cla(gca);
+    V = V + 0.2*L;
+    hold on;
+    trisurf(F, V(:, 1), V(:, 2), V(:, 3));%,'FaceColor',[0.26,0.33,1.0 ]);
+    hold off;
+    view(2)
+    L = calc_laplacian('cotan', V, F, FxV, VxV);
+    drawnow;
+    %pause(0.1)
+end
+
+
 %quiver3(V(:, 1), V(:, 2), V(:, 3), N(:, 1), N(:, 2), N(:, 3))
 %quiver3(V(:, 1), V(:, 2), V(:, 3), L(:, 1), L(:, 2), L(:, 3));
 
 %add light sources
 %light('Position', [0, 60.0, 30.0], 'Style', 'infinite');
 %light('Position', [0, -60.0, -30.0], 'Style', 'infinite');
-
-lighting phong;
+%lighting phong;
